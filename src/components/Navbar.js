823413338469a63422dc5ec6f6a4cca0958bdb15
tejaskasare08 <tejas@itvedant.com>
2025-01-24 18:38:35 +0530

    react api intergrated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to={'/'}>Bootcamp-App</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to={'/'}>Bootcamp-App</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
-          <Link class="nav-link active" aria-current="page" to={'/'}>Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+          <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to={'/products'}>Products</Link>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to={'/products'}>Products</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to={'/contact'}>Contact</Link>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to={'/contact'}>Contact</Link>
         </li>
       </ul>
     </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Products() {
-  return (
-    <div>
-      product comp
-    </div>
-  )
-}
+// export default function Products() {
+// // let counter=0
+// // when variable value/state changes please re-render my component ==> useState() do that work for you
+// // let [variable_to _store_initinal_state, function_to_change_state/value]=useState(initalValue)
+
+// let [counter,setCounter]=useState(0)
+
+//   const fetchProducts = async()=>
+//     {
+//     let response=await fetch('https://dummyjson.com/products')
+//     let productsData=await response.json()
+//     console.log(productsData);
+//     console.log(productsData.products);
+    
+//   }
+//   //fetchProducts()
+
+//   useEffect(()=>{
+//     fetchProducts()
+//   },[counter])
+//   const changeCounter=()=>
+//   {
+//     counter = counter+=1
+//     console.log("counter value is: ", counter);
+//     setCounter(counter)
+//   }
+//   return (
+//     <div>
+//       <p>counter value is : {counter}</p>
+//       <button onClick={changeCounter}>click me to change</button>
+//     </div>
+//   )
+// }
+
+export default function Products() 
+ {
+  let [products,setProducts]=useState(null)
+  console.log(products);
+  
+    const fetchProducts = async()=>
+      {
+      let response=await fetch('https://dummyjson.com/products')
+      let productsData=await response.json()
+      console.log(productsData);
+      console.log(productsData.products);
+      setProducts(productsData.products) 
+    }
+
+    useEffect(()=>{
+      fetchProducts()
+    },[])
+    
+    return (
+      <div>
+        
+          {/* products && products.map(product=>{console.log(product.id)}) */}
+          <div className='container'>
+          <div className='row'>
+              {
+                    products && products.map(product=>{
+                    return <>
+                    
+                          <div className='col-3'>
+                            <div class="card" style={{width:16+"rem"}}>
+                              <img src={product.thumbnail} class="card-img-top" alt="..."/>
+                              <div class="card-body">
+                                <h5 class="card-title">{product.title.slice(0,15)}</h5>
+                                <p class="card-text">{product.description.slice(0,70)}...</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+              })
+            }
+        </div>
+        </div>
+      </div>
+    )
+  }
